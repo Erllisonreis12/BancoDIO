92fe8ca1d9919faa1ac564dea7aecdc56e06c487
Erllison Reis <erllisonreis12@gmail.com>
2021-10-20 16:03:19 -0400

    inicio

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoTeste
+{
+    class Program
+    {
+        public static void  Main (string[] args) 
+        {
+            while(true){
+                String senha = Console.ReadLine();
+                if(String.IsNullOrEmpty(senha)) break;
+
+                var vetor = senha.ToCharArray(0,senha.Length);
+                bool cont = false;
+
+                foreach(char v in vetor){
+                    if(Char.IsUpper(v)){cont = true; break;};
+        
+                }
+                //Console.WriteLine(cont);
+                if( (senha.Length < 6 && senha.Length < 32) ||
+                    (senha.Any(ch => Char.IsPunctuation(ch) || Char.IsSeparator(ch))) ||
+                    !cont
+                ){
+                    Console.WriteLine("Senha Inválida");
+                }else{
+                    Console.WriteLine("Senha Válida");
+                }
+            }    
+        }
+    }
+}
